0f5c6c53176eb5b1649ab4f2dbf189e1d791cc77
UsefulTornado <Azarel.url@gmail.com>
2022-11-25 19:56:19 +0300

    added new options

--- cfg_normalization/main.py
+++ cfg_normalization/main.py
@@ -1,20 +1,34 @@
 import argparse
 
-from cfg_parser import Parser
+from bisimulation import Bisimulation
+from blum_koch import blum_koch_normalize
 from cfg import CFGrammar
-from blum_koch import blum_koch
+from cfg_parser import Parser
 
 
 def main():
-    parser = argparse.ArgumentParser(description="")
+    parser = argparse.ArgumentParser(description="Normalization of a CFG.")
     parser.add_argument(
-        "-f", "--file_path", help="Path to the file with CFG input data.",
+        "-f", "--file_path",
+        help="Path to the file with context-free grammar input data.",
     )
     parser.add_argument(
-        "-bk", "--blum_koch", action="store_true", help="Whether to use Blum-Koch algorithm.",
+        "-cnf", "--chomsky", action="store_true",
+        help="Whether to use Chomsky normalization."
+             "If not specified, Greibach normalization is used.",
     )
     parser.add_argument(
-        "-v", "--verbose", action="store_true", help="Whether to print additional information.",
+        "-bk", "--blum_koch", action="store_true",
+        help="Whether to use Blum-Koch algorithm for Greibach normalization."
+             "If not specified, elimination of left recursion is used.",
+    )
+    parser.add_argument(
+        "-bs", "--bisimulate", action="store_true",
+        help="Whether to simplify grammar with bisimulation.",
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="store_true",
+        help="Whether to print additional information.",
     )
     args = parser.parse_args()
 
@@ -23,29 +37,27 @@ def main():
 
     cfg = CFGrammar(rules=Parser().parse_grammar(lines))
 
-    if args.blum_koch:
-        greibach_normalized_cfg, nfas = blum_koch(cfg)
-        print(f'Starting: {greibach_normalized_cfg.start}')
-        print('Nonterminals:', *greibach_normalized_cfg.nonterminals)
-        print('\nRules:\n')
-        for rule in greibach_normalized_cfg.rules:
-            print(rule)
-
-        if args.verbose:
-            print('\nNFAs for sentential forms:')
-            for nfa in nfas:
-                if nfa.transitions:
-                    print()
-                    print(nfa.nonterminal)
-                    for tr in nfa.transitions:
-                        print(tr)
+    if args.chomsky:
+        normalized_cfg = CFGrammar.to_chomsky_normal_form(cfg)
+        print(normalized_cfg)
     else:
-        greibach_normalized_cfg, ordered_nonterminals = CFGrammar.to_greibach_normal_form(cfg)
-        print(f'Starting: {greibach_normalized_cfg.start}')
-        print('Ordered nonterminals:', *ordered_nonterminals)
-        print('\nRules:\n')
-        for rule in greibach_normalized_cfg.rules:
-            print(rule)
+        if args.blum_koch:
+            normalized_cfg, nfas = blum_koch_normalize(cfg)
+            print(normalized_cfg)
+            if args.verbose:
+                print('\nNFAs for sentential forms:\n')
+                for nfa in nfas:
+                    print(nfa, end='\n\n')
+        else:
+            normalized_cfg, ordered_nonterminals = CFGrammar.to_greibach_normal_form(cfg)
+            print(normalized_cfg)
+            if args.verbose:
+                print('\nOrder of nonterminals:',
+                      ' > '.join(map(str, ordered_nonterminals)))
+
+    if args.bisimulate:
+        print('\nBisimilar grammar:\n\n',
+              Bisimulation(normalized_cfg).get_bisimilar_grammar(), sep='')
 
 
 if __name__ == "__main__":
