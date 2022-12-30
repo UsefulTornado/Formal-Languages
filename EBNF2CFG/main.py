162d3d9f668896a7b5b2a8dd932f195c370eb167
Espenario <golovkin.dima@list.ru>
2022-12-30 18:11:02 +0300

    ebnf_2_cfg + visualisation

--- EBNF2CFG/main.py
+++ EBNF2CFG/main.py
@@ -2,6 +2,7 @@ import argparse
 import json
 
 from grammar_parser import Lexer, Parser
+from ebnf_2_cfg import Converter
 
 
 def main():
@@ -14,6 +15,10 @@ def main():
         "-g", "--grammar",
         help="Path to the file with grammar in custom EBNF syntax.",
     )
+    parser.add_argument(
+        "-sc", "--syntax_cfg", default="EBNF2CFG/cfg_syntax.json",
+        help="Path to the file with grammar in custom CFG syntax.",
+    )
     args = parser.parse_args()
 
     with open(args.syntax, mode="r", encoding="utf-8") as file:
@@ -22,8 +27,18 @@ def main():
     with open(args.grammar, mode="r", encoding="utf-8") as file:
         grammar = file.read()
 
+    with open(args.syntax_cfg, mode="r", encoding="utf-8") as file:
+        syntax_cfg = json.load(file)
+
     tokens = Lexer(syntax).tokenize(grammar)
     rules = Parser().parse(tokens)
+
+    print(rules[0].rhs)
+    cucumber = Converter(rules, [])
+    cfg = cucumber.ebnf_2_cfg()
+    for rule in cfg.rules:
+        print(rule.lhs, rule.rhs)
+    cucumber.display_cfg_in_user_syntax(syntax_cfg)
 
 if __name__ == "__main__":
     main()
